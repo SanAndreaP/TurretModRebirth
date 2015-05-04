16812bcfe71eb432017c5029f025394f8c3fc6ed
SanAndreasP <1294@live.de>
2015-05-05 00:10:32 +0200

    info page now renders the ammo item

--- java/de/sanandrew/mods/turretmod/client/gui/tcu/GuiTcuInfo.java
+++ java/de/sanandrew/mods/turretmod/client/gui/tcu/GuiTcuInfo.java
@@ -11,12 +11,20 @@ package de.sanandrew.mods.turretmod.client.gui.tcu;
 import de.sanandrew.core.manpack.util.helpers.SAPUtils;
 import de.sanandrew.mods.turretmod.entity.turret.AEntityTurretBase;
 import de.sanandrew.mods.turretmod.util.EnumTextures;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 public class GuiTcuInfo
         extends AGuiTurretControlUnit
 {
     private int specOwnerHead;
+
+    private static RenderItem itemRenderer = new RenderItem();
 
     public GuiTcuInfo(AEntityTurretBase turret) {
         super(turret);
@@ -47,16 +55,48 @@ public class GuiTcuInfo
         value = String.format("%.1f / %.1f HP", this.myTurret.getHealth(), this.myTurret.getMaxHealth());
         this.fontRendererObj.drawString(value, this.guiLeft + 20, this.guiTop + 35, 0x000000);
 
-        value = String.format("%d / %d proj.", this.myTurret.getAmmo(), this.myTurret.getMaxAmmo());
-        this.fontRendererObj.drawString(value, this.guiLeft + 20, this.guiTop + 47, 0x000000);
+        value = String.format("%d", this.myTurret.getAmmo());
 
-        value = this.myTurret.getAmmoType() != null ? this.myTurret.getAmmoType().getTypeItem().getDisplayName() : "-n/a-";
-        this.fontRendererObj.drawString(value, this.guiLeft + 20, this.guiTop + 59, 0x000000);
-
+        GL11.glColor3f(1.0F, 1.0F, 1.0F);
+        this.drawIcon(this.myTurret.getAmmoType().getTypeItem().getIconIndex(), this.guiLeft + 21, this.guiTop + 49);
+        GL11.glTranslated(0.0D, 0.0D, 300.0D);
+        this.fontRendererObj.drawStringWithShadow(value, this.guiLeft + 38 - this.fontRendererObj.getStringWidth(value), this.guiTop + 58, 0xFFFFFFFF);
+        GL11.glTranslated(0.0D, 0.0D, -300.0D);
+//        this.zLevel = 0.0F;
+//        value = this.myTurret.getAmmoType() != null ? this.myTurret.getAmmoType().getTypeItem().getDisplayName() : "-n/a-";
+//        this.fontRendererObj.drawString(value, this.guiLeft + 20, this.guiTop + 59, 0x000000);
+//
         value = this.myTurret.hasTarget() ? SAPUtils.translatePreFormat("entity.%s.name", this.myTurret.getTargetName()) : "-n/a-";
         this.fontRendererObj.drawString(value, this.guiLeft + 20, this.guiTop + 71, 0x000000);
 
         value = this.myTurret.getOwnerName();
         this.fontRendererObj.drawString(value, this.guiLeft + 20, this.guiTop + 95, 0x000000);
+    }
+
+    private void drawIcon(IIcon icon, int posX, int posY) {
+        GL11.glPushMatrix();
+        GL11.glTranslatef(0.0F, 0.0F, 32.0F);
+        this.zLevel = 200.0F;
+        itemRenderer.zLevel = 200.0F;
+
+        GL11.glEnable(GL11.GL_BLEND);
+        OpenGlHelper.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ZERO);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
+
+        ResourceLocation resourcelocation = this.mc.getTextureManager().getResourceLocation(1);
+        mc.getTextureManager().bindTexture(resourcelocation);
+
+        if( icon == null ) {
+            icon = ((TextureMap) Minecraft.getMinecraft().getTextureManager().getTexture(resourcelocation)).getAtlasSprite("missingno");
+        }
+
+        itemRenderer.renderIcon(posX, posY, icon, 16, 16);
+
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        GL11.glDisable(GL11.GL_BLEND);
+
+        this.zLevel = 0.0F;
+        itemRenderer.zLevel = 0.0F;
+        GL11.glPopMatrix();
     }
 }
