6df142efc32db4598763c25e0db58a161583018e
SanAndreasP <1294@live.de>
2017-03-16 09:59:29 +0100

    fixed turret cam renderer to constantly reset the current screen, even if there's no turret cam active

--- src/main/java/de/sanandrew/mods/turretmod/client/render/world/RenderTurretCam.java
+++ src/main/java/de/sanandrew/mods/turretmod/client/render/world/RenderTurretCam.java
@@ -49,98 +49,101 @@ public class RenderTurretCam
             return;
         }
 
-        if( !mc.inGameHasFocus ) {
-            prevDisplayedGui = mc.currentScreen;
-            mc.currentScreen = null;
-            mc.inGameHasFocus = true;
-        }
+        if( TURRETS.size() > 0 ) {
 
-        cleanupRenderers(false);
-
-        TURRETS.forEach((turret, camEntry) -> {
-            if( turret != null ) {
-                long updTime = System.nanoTime();
-                if( updTime - camEntry.lastUpdTime > MAX_UPDATE_TIME_NS ) {
-                    GameSettings settings = mc.gameSettings;
-                    Entity entityBkp = mc.getRenderViewEntity();
-                    int thirdPersonBkp = settings.thirdPersonView;
-                    boolean hideGuiBkp = settings.hideGUI;
-                    int mipmapBkp = settings.mipmapLevels;
-                    float fovBkp = settings.fovSetting;
-                    int widthBkp = mc.displayWidth;
-                    int heightBkp = mc.displayHeight;
-                    float turretPrevYawBkp = turret.prevRotationYaw;
-                    float turretYawBkp = turret.rotationYaw;
-                    float turretPitchBkp = turret.rotationPitch;
-                    float turretPrevPitchBkp = turret.prevRotationPitch;
-                    double turretPosYBkp = turret.posY;
-                    double turretPrevPosYBkp = turret.prevPosY;
-                    double turretLTPosYBkp = turret.lastTickPosY;
-
-                    mc.setRenderViewEntity(turret);
-                    settings.fovSetting = 100.0F;
-                    settings.thirdPersonView = 0;
-                    settings.hideGUI = true;
-                    settings.mipmapLevels = 0;
-                    mc.displayWidth = QUALITY;
-                    mc.displayHeight = QUALITY;
-
-                    RenderEventHandler.renderPlayer = true;
-                    RenderEventHandler.renderEntity = mc.player;
-
-                    turret.prevRotationYaw = turret.prevRotationYawHead;
-                    turret.rotationYaw = turret.rotationYawHead;
-                    if( turret.isUpsideDown ) {
-                        turret.posY -= 1.0F;
-                        turret.prevPosY -= 1.0F;
-                        turret.lastTickPosY -= 1.0F;
-                        turret.rotationPitch += 180.0F;
-                        turret.prevRotationPitch += 180.0F;
-                        turret.rotationYaw = -turret.rotationYawHead;
-                        turret.prevRotationYaw = -turret.prevRotationYawHead;
-                    }
-
-                    int fps = Math.min(Minecraft.getDebugFPS(), mc.gameSettings.limitFramerate);
-                    fps = Math.max(fps, 60);
-                    long timeDelta = updTime - renderEndNanoTime;
-                    long timeDeltaFps = Math.max((1000000000 / fps / 4) - timeDelta, 0L);
-
-                    mc.entityRenderer.renderWorld(renderTickTime, System.nanoTime() + timeDeltaFps);
-                    GlStateManager.bindTexture(camEntry.textureId);
-                    GL11.glCopyTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, 0, 0, QUALITY, QUALITY, 0);
-
-                    renderEndNanoTime = System.nanoTime();
-
-                    turret.posY = turretPosYBkp;
-                    turret.prevPosY = turretPrevPosYBkp;
-                    turret.lastTickPosY = turretLTPosYBkp;
-                    turret.rotationYaw = turretYawBkp;
-                    turret.prevRotationYaw = turretPrevYawBkp;
-                    turret.rotationPitch = turretPitchBkp;
-                    turret.prevRotationPitch = turretPrevPitchBkp;
-
-                    RenderEventHandler.renderEntity = null;
-                    RenderEventHandler.renderPlayer = false;
-
-                    //noinspection ConstantConditions
-                    mc.setRenderViewEntity(entityBkp);
-                    settings.fovSetting = fovBkp;
-                    settings.thirdPersonView = thirdPersonBkp;
-                    settings.hideGUI = hideGuiBkp;
-                    settings.mipmapLevels = mipmapBkp;
-                    mc.displayWidth = widthBkp;
-                    mc.displayHeight = heightBkp;
-
-                    camEntry.lastUpdTime = updTime;
-                }
+            if (!mc.inGameHasFocus) {
+                prevDisplayedGui = mc.currentScreen;
+                mc.currentScreen = null;
+                mc.inGameHasFocus = true;
             }
 
-            camEntry.active = false;
-        });
+            cleanupRenderers(false);
 
-        if( prevDisplayedGui != null ) {
-            mc.currentScreen = prevDisplayedGui;
-            mc.inGameHasFocus = false;
+            TURRETS.forEach((turret, camEntry) -> {
+                if (turret != null) {
+                    long updTime = System.nanoTime();
+                    if (updTime - camEntry.lastUpdTime > MAX_UPDATE_TIME_NS) {
+                        GameSettings settings = mc.gameSettings;
+                        Entity entityBkp = mc.getRenderViewEntity();
+                        int thirdPersonBkp = settings.thirdPersonView;
+                        boolean hideGuiBkp = settings.hideGUI;
+                        int mipmapBkp = settings.mipmapLevels;
+                        float fovBkp = settings.fovSetting;
+                        int widthBkp = mc.displayWidth;
+                        int heightBkp = mc.displayHeight;
+                        float turretPrevYawBkp = turret.prevRotationYaw;
+                        float turretYawBkp = turret.rotationYaw;
+                        float turretPitchBkp = turret.rotationPitch;
+                        float turretPrevPitchBkp = turret.prevRotationPitch;
+                        double turretPosYBkp = turret.posY;
+                        double turretPrevPosYBkp = turret.prevPosY;
+                        double turretLTPosYBkp = turret.lastTickPosY;
+
+                        mc.setRenderViewEntity(turret);
+                        settings.fovSetting = 100.0F;
+                        settings.thirdPersonView = 0;
+                        settings.hideGUI = true;
+                        settings.mipmapLevels = 0;
+                        mc.displayWidth = QUALITY;
+                        mc.displayHeight = QUALITY;
+
+                        RenderEventHandler.renderPlayer = true;
+                        RenderEventHandler.renderEntity = mc.player;
+
+                        turret.prevRotationYaw = turret.prevRotationYawHead;
+                        turret.rotationYaw = turret.rotationYawHead;
+                        if (turret.isUpsideDown) {
+                            turret.posY -= 1.0F;
+                            turret.prevPosY -= 1.0F;
+                            turret.lastTickPosY -= 1.0F;
+                            turret.rotationPitch += 180.0F;
+                            turret.prevRotationPitch += 180.0F;
+                            turret.rotationYaw = -turret.rotationYawHead;
+                            turret.prevRotationYaw = -turret.prevRotationYawHead;
+                        }
+
+                        int fps = Math.min(Minecraft.getDebugFPS(), mc.gameSettings.limitFramerate);
+                        fps = Math.max(fps, 60);
+                        long timeDelta = updTime - renderEndNanoTime;
+                        long timeDeltaFps = Math.max((1000000000 / fps / 4) - timeDelta, 0L);
+
+                        mc.entityRenderer.renderWorld(renderTickTime, System.nanoTime() + timeDeltaFps);
+                        GlStateManager.bindTexture(camEntry.textureId);
+                        GL11.glCopyTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGB, 0, 0, QUALITY, QUALITY, 0);
+
+                        renderEndNanoTime = System.nanoTime();
+
+                        turret.posY = turretPosYBkp;
+                        turret.prevPosY = turretPrevPosYBkp;
+                        turret.lastTickPosY = turretLTPosYBkp;
+                        turret.rotationYaw = turretYawBkp;
+                        turret.prevRotationYaw = turretPrevYawBkp;
+                        turret.rotationPitch = turretPitchBkp;
+                        turret.prevRotationPitch = turretPrevPitchBkp;
+
+                        RenderEventHandler.renderEntity = null;
+                        RenderEventHandler.renderPlayer = false;
+
+                        //noinspection ConstantConditions
+                        mc.setRenderViewEntity(entityBkp);
+                        settings.fovSetting = fovBkp;
+                        settings.thirdPersonView = thirdPersonBkp;
+                        settings.hideGUI = hideGuiBkp;
+                        settings.mipmapLevels = mipmapBkp;
+                        mc.displayWidth = widthBkp;
+                        mc.displayHeight = heightBkp;
+
+                        camEntry.lastUpdTime = updTime;
+                    }
+                }
+
+                camEntry.active = false;
+            });
+
+            if (prevDisplayedGui != null) {
+                mc.currentScreen = prevDisplayedGui;
+                mc.inGameHasFocus = false;
+            }
         }
     }
 
