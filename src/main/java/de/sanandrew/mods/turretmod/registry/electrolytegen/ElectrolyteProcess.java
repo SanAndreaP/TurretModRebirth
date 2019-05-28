f5ff4093ffa862e55d75460a36fd84f35a9effd8
SanAndreasP <sanandreaspm@gmail.com>
2019-05-28 22:05:34 +0200

    fixed NPE crash for Electrolyte Generators loaded from before the recipe rewrite

--- src/main/java/de/sanandrew/mods/turretmod/registry/electrolytegen/ElectrolyteProcess.java
+++ src/main/java/de/sanandrew/mods/turretmod/registry/electrolytegen/ElectrolyteProcess.java
@@ -12,8 +12,11 @@ import de.sanandrew.mods.turretmod.api.electrolytegen.IElectrolyteRecipe;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 
 public class ElectrolyteProcess
@@ -26,20 +29,20 @@ public class ElectrolyteProcess
     private short progress = 0;
 
     public ElectrolyteProcess(IElectrolyteRecipe recipe, ItemStack stack) {
-        this.recipe = recipe;
+        this.recipe = MiscUtils.defIfNull(recipe, InvalidRecipe.INSTANCE);
         this.processStack = stack;
     }
 
     public ElectrolyteProcess(ByteBuf buf) {
         this.processStack = ByteBufUtils.readItemStack(buf);
         this.progress = buf.readShort();
-        this.recipe = ElectrolyteManager.INSTANCE.getFuel(new ResourceLocation(ByteBufUtils.readUTF8String(buf)));
+        this.recipe = MiscUtils.defIfNull(ElectrolyteManager.INSTANCE.getFuel(new ResourceLocation(ByteBufUtils.readUTF8String(buf))), InvalidRecipe.INSTANCE);
     }
 
     public ElectrolyteProcess(NBTTagCompound nbt) {
         this.processStack = new ItemStack(nbt.getCompoundTag("ProgressItem"));
         this.progress = nbt.getShort("Progress");
-        this.recipe = ElectrolyteManager.INSTANCE.getFuel(new ResourceLocation(nbt.getString("Recipe")));
+        this.recipe = MiscUtils.defIfNull(ElectrolyteManager.INSTANCE.getFuel(new ResourceLocation(nbt.getString("Recipe"))), InvalidRecipe.INSTANCE);
     }
 
     public void writeToByteBuf(ByteBuf buf) {
@@ -80,5 +83,44 @@ public class ElectrolyteProcess
 
     public boolean hasFinished() {
         return this.progress >= this.recipe.getProcessTime();
+    }
+
+    private static final class InvalidRecipe
+        implements IElectrolyteRecipe
+    {
+        public static final InvalidRecipe INSTANCE = new InvalidRecipe();
+
+        @Override
+        public ResourceLocation getId() { return null; }
+
+        @Override
+        public float getEfficiency() { return 0; }
+
+        @Override
+        public int getProcessTime() { return 0; }
+
+        @Override
+        public boolean matches(IInventory inv, World worldIn) { return false; }
+
+        @Override
+        public ItemStack getCraftingResult(IInventory inv) { return null; }
+
+        @Override
+        public ItemStack getTreasureResult(IInventory inv) { return null; }
+
+        @Override
+        public ItemStack getRecipeOutput() { return null; }
+
+        @Override
+        public NonNullList<Ingredient> getIngredients() { return null; }
+
+        @Override
+        public String getGroup() { return null; }
+
+        @Override
+        public float getTrashChance() { return 0; }
+
+        @Override
+        public float getTreasureChance() { return 0; }
     }
 }
