a5b2a05eeda76b30005af6db5062c0e48018396a
SanAndreasP <sanandreaspm@gmail.com>
2021-02-13 00:48:42 +0100

    * updated Forge and Patchouli version  * Turret Crates emissive parts don't have a shadow anymore

--- src/main/java/de/sanandrew/mods/turretmod/api/TmrConstants.java
+++ src/main/java/de/sanandrew/mods/turretmod/api/TmrConstants.java
@@ -17,7 +17,7 @@ public final class TmrConstants
     public static final String VERSION = "4.0.0-beta.4";
     public static final String CHANNEL = "SapTurretModNWCH";
     public static final String NAME = "Turret Mod Rebirth";
-    public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2768,];required-after:sanlib@[1.6.0,]";
+    public static final String DEPENDENCIES = "required-after:forge@[14.23.5.2847,];required-after:sanlib@[1.6.0,]";
 
     public static final String API_ID = "sapturretmod_api";
     public static final String API_VERSION = "1.2.0-beta.4";
