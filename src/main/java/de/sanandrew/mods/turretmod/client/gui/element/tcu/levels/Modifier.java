50186b8ed6adef1b80db345bc72bcb1db89ebcfb
SanAndreasP <sanandreaspm@gmail.com>
2022-03-06 23:42:08 +0100

    started work on modifier list within levels TCU GUI

--- src/main/java/de/sanandrew/mods/turretmod/client/gui/element/tcu/levels/Modifier.java
+++ src/main/java/de/sanandrew/mods/turretmod/client/gui/element/tcu/levels/Modifier.java
@@ -1,8 +1,16 @@
 package de.sanandrew.mods.turretmod.client.gui.element.tcu.levels;
 
+import com.google.gson.JsonObject;
+import de.sanandrew.mods.sanlib.lib.client.gui.GuiElementInst;
+import de.sanandrew.mods.sanlib.lib.client.gui.IGui;
+import de.sanandrew.mods.sanlib.lib.client.gui.element.DynamicText;
 import de.sanandrew.mods.sanlib.lib.client.gui.element.ElementParent;
+import de.sanandrew.mods.sanlib.lib.client.gui.element.Texture;
+import de.sanandrew.mods.sanlib.lib.util.JsonUtils;
+import de.sanandrew.mods.sanlib.lib.util.MiscUtils;
 import de.sanandrew.mods.turretmod.item.upgrades.leveling.Stage;
 import net.minecraft.entity.ai.attributes.Attribute;
+import org.apache.commons.lang3.Range;
 
 public class Modifier
         extends ElementParent<String>
@@ -11,5 +19,25 @@ public class Modifier
     private static final String LABEL = "label";
     private static final String VALUE = "value";
 
-    private final Attribute attrib;
+//    private final Attribute attrib;
+
+    public static class Builder
+            implements IBuilder<Modifier>
+    {
+        @Override
+        public void sanitize(IGui iGui) { }
+
+        protected GuiElementInst loadBackground(IGui gui, JsonObject bgData) {
+            return new GuiElementInst(JsonUtils.getIntArray(bgData.get("offset"), new int[2], Range.is(2)), Texture.Builder.fromJson(gui, bgData));
+        }
+
+        protected GuiElementInst loadLabel(IGui gui, JsonObject lblData) {
+            return new GuiElementInst(JsonUtils.getIntArray(lblData.get("offset"), new int[2], Range.is(2)), DynamicText.Builder.fromJson(gui, lblData));
+        }
+
+        @Override
+        public Modifier get(IGui iGui) {
+            return null;
+        }
+    }
 }
--- src/main/java/de/sanandrew/mods/turretmod/client/gui/element/tcu/levels/ModifierList.java
+++ src/main/java/de/sanandrew/mods/turretmod/client/gui/element/tcu/levels/ModifierList.java
@@ -1,5 +1,6 @@
 package de.sanandrew.mods.turretmod.client.gui.element.tcu.levels;
 
+import com.google.gson.JsonObject;
 import de.sanandrew.mods.sanlib.lib.client.gui.IGui;
 import de.sanandrew.mods.sanlib.lib.client.gui.element.ScrollButton;
 import de.sanandrew.mods.turretmod.client.gui.element.StackedScrollArea;
@@ -7,7 +8,10 @@ import de.sanandrew.mods.turretmod.client.gui.element.StackedScrollArea;
 public class ModifierList
         extends StackedScrollArea
 {
-    public ModifierList(int[] areaSize, int scrollHeight, boolean rasterized, float maxScrollDelta, int[] scrollbarPos, ScrollButton scrollButton, IGui gui) {
-        super(areaSize, scrollHeight, rasterized, maxScrollDelta, scrollbarPos, scrollButton, gui);
+    final JsonObject modData;
+
+    public ModifierList(int[] areaSize, int scrollHeight, float maxScrollDelta, int[] scrollbarPos, ScrollButton scrollButton, IGui gui, JsonObject modData) {
+        super(areaSize, scrollHeight, true, maxScrollDelta, scrollbarPos, scrollButton, gui);
+        this.modData = modData;
     }
 }
